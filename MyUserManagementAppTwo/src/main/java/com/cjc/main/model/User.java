bfd88fddd66f6d4da11eeac0df15b98557155460
rockbandstyle123 <rockbandstyle123@gmail.com>
2025-03-08 17:04:46 +0530

    Generated getter and setter for user class fields

--- MyUserManagementAppTwo/src/main/java/com/cjc/main/model/User.java
+++ MyUserManagementAppTwo/src/main/java/com/cjc/main/model/User.java
@@ -6,5 +6,30 @@ public class User {
 	private String userName;
 	private float age;
 	private String category;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public float getAge() {
+		return age;
+	}
+	public void setAge(float age) {
+		this.age = age;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
+	
 }
